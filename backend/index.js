27eab2be409d0c1f66565c2905b4378e15938896
pratik patil <pratikp3800@gmail.com>
2024-07-30 21:37:08 +0530

    second

--- backend/index.js
+++ backend/index.js
@@ -4,28 +4,28 @@ const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const authRouter = require("./routers/authRouter");
+const {
+  sessionMiddleware,
+  wrap,
+  corsConfig,
+} = require("./controlers/servercontrol");
 
 const server = require("http").createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    credentials: "true",
-  },
+  cors: corsConfig,
 });
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors(corsConfig));
 app.use(express.json());
 
+io.use(wrap(sessionMiddleware));
 app.use("/auth", authRouter);
-
-io.on("connect", socket => {});
+io.on("connect", socket => {
+  console.log(socket.id);
+  console.log(socket.request.session.user.username);
+});
 
 server.listen(4000, () => {
   console.log("Server listening on port 4000");
